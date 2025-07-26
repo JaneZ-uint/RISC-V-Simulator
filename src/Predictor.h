31445a76de998c35ef79043b416178cb95e17c89
gyhyz917 <zhuyihan0826@outlook.com>
2025-07-26 21:36:27 +0800

    feat:finish predictor.

--- src/Predictor.h
+++ src/Predictor.h
@@ -1,6 +1,65 @@
 #pragma once
+#include "Type.h"
+#include "reg.h"
+
 namespace JaneZ{
 class Predictor{
+struct BranchInfo{
+    int pc;
+    int offset;
+    Operation op;
+    int serial;
+    Reg<bool> flag;
 
+    void tick(){
+        flag.flush();
+    }
+};
+
+struct ALUInfo{
+    int serial;
+    int res;
+};
+
+struct PredictorOutput{
+    int new_pc;
+    int serial;
+};
+
+public:
+    BranchInfo predictor[1000];
+    BranchInfo input;
+    ALUInfo inputALU;
+    PredictorOutput output;
+    bool isAdd;
+    bool isALU;
+    
+    void run(){
+        if(isAdd){
+            predictor[input.serial] = input;
+        }
+        if(isALU){
+            if(inputALU.res == 1){
+                //wrong
+                output.new_pc = input.pc + input.offset;
+                output.serial = input.serial;
+                flush();
+            }
+        }
+    }
+
+    void flush(){
+        for(int i = output.serial;i < 1000;i ++){
+            if(predictor[i].flag.current){
+                predictor[i].flag.update(false);
+            }
+        }
+    }
+
+    void tick(){
+        for(int i = 0;i < 1000;i ++){
+            predictor[i].tick();
+        }
+    }
 };
 }
