84c963e252b01132d5f7beb15908afc2870a2d60
gyhyz917 <zhuyihan0826@outlook.com>
2025-07-26 21:32:57 +0800

    feat:finish RoB.

--- src/RoB.h
+++ src/RoB.h
@@ -1,36 +1,114 @@
 #pragma once
 #include "Queue.h"
+#include "RS.h"
+#include "Type.h"
 #include "reg.h"
 
 namespace JaneZ{
 struct RoBInfo{
     int serial;
-    bool isBusy = false;
-    bool isReady = false;
+    Reg<bool> isBusy = {false,false};
+    Reg<bool> isReady = {false,false};
     int des;// target reg
+    Reg<int> value;
+    Operation op;
+    int pc;
+    int imm;
+
+    void tick(){
+        isBusy.flush();
+        isReady.flush();
+        value.flush();
+    }
+};
+struct RoBOutput{
+    int des1;
+    int serial1;
+    int des2;
+    int serial2;
     int value;
 };
 
 class RoB{
+private:
+    void enQueue(){
+        if(isAdd){
+            if(input.op == JAL){
+                output.des2 = input.des;
+                output.serial2 = input.serial;
+                input.value.update(input.pc + 4);
+                rob[tail.current] = input;
+                tail.update((tail.current + 1)%1000);
+            }else if(input.op == AUIPC){
+                output.des2 = input.des;
+                output.serial2 = input.serial;
+                int res = input.imm << 12;
+                input.value.update(input.pc + res);
+                rob[tail.current] = input;
+                tail.update((tail.current + 1)%1000);
+            }else if(input.op == LUI){
+                output.des2 = input.des;
+                output.serial2 = input.serial;
+                int res = input.imm << 12;
+                input.value.update(res);
+                rob[tail.current] = input;
+                tail.update((tail.current + 1)%1000);
+            }else{
+                output.des2 = input.des;
+                output.serial2 = input.serial;
+                rob[tail.current] = input;
+                tail.update((tail.current + 1)%1000);
+            }
+        }
+    }
+
+    void deQue(){
+        if(!rob[head.current].isBusy.current && rob[head.current].isReady.current){
+            output.des1 = rob[head.current].des;
+            output.serial1 = rob[head.current].serial;
+            output.value = rob[head.current].value.current;
+            head.update((head.current + 1)%1000);
+        }
+    }
+
+    void update(){
+        if(InfoFromRS.flag){
+            for(int i = 0;i < 1000;i ++){
+                if(rob[i].serial == InfoFromRS.serial){
+                    rob[i].value.update(InfoFromRS.res);
+                    rob[i].isReady.update(true);
+                    break;
+                }
+            }
+        }
+    }
+
 public:
-    Queue<RoBInfo> rob;
-    RoBInfo toAdd; //待加入指令
-    RoBInfo toLeave; //出队指令
-    Reg<RoBInfo> buffer[100];
+    RoBInfo rob[1000];
+    Reg<int> head = {0,0};
+    Reg<int> tail = {0,0};
+    RoBInfo input;
+    RoBOutput output;
+    bool isAdd;
+    RSOutput InfoFromRS;
+
 
     void run(){
-        if(!rob.isFull() && toAdd.isBusy){
-            rob.push(toAdd);
+        enQueue();
+        deQue();
+        update();
+    }
+
+    void tick(){
+        for(int i = 0;i < 1000;i ++){
+            rob[i].tick();
         }
-        if(toLeave.isBusy){
-            return;
-        }
-        RoBInfo head = rob.front();
-        if(head.isBusy && head.isReady){
-            head.isBusy = false;
-            rob.pop();
-            toLeave = head;
-        }
+        head.flush();
+        tail.flush();
+    }
+
+    void flush(){
+        //TODO
     }
 };
 }
