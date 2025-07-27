c655a495d005b593558f539906b08f424fb3ecd1
gyhyz917 <zhuyihan0826@outlook.com>
2025-07-27 20:49:13 +0800

    feat:add output.h for some structs.

---
+++ src/output.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace JaneZ {
+    struct RoBOutput{
+        int des1;
+        int serial1;
+        int value;
+        bool flag1;
+    
+        int des2;
+        int serial2;
+        bool flag2;
+    };
+    
+    struct RSOutput{
+        int serial;
+        int res;
+        bool flag;
+    };
+}
